2906076e06b21d7faf50d77cb92e899dcc5c038e
GORYNTCHdotNET <92940275+GORYNTCHdotNET@users.noreply.github.com>
2022-03-27 23:34:56 +0300

    FirstDay

---
+++ Meetup.Domain/Meetup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Meetups.Domain
+{
+    public class Meetup
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Speaker { get; set; }
+        public string Place { get; set; }
+        public DateTime MeetupDate { get; set; }
+    }
+}
